9c3b8e51d6dad126f388c64bef2424b8d812a64f
Justin Fernandez <136839185+justinfernz@users.noreply.github.com>
2024-03-21 10:33:18 -0400

    Suggestions for project solution

--- model/todo.js
+++ model/todo.js
@@ -1,7 +1,7 @@
 import pool from './database';
 
-export const create = async (description) => {
-    try {
+export const create = async (description) => { //does this have to be an async function?
+    try { //is there a way to write this without try/catch?
         const result = await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [
             description,
         ]);
@@ -12,7 +12,7 @@ export const create = async (description) => {
 };
 
 export const get = async () => {
-    try {
+    try { //code can be simplified (hint: try/catch & async may not be needed)
         const result = await pool.query('SELECT * FROM todo');
         return result.rows; // Assuming you want to return all rows
     } catch (error) {
@@ -21,9 +21,11 @@ export const get = async () => {
 };
 
 export const removeTodo = async (id) => {
-    try {
+    try { //same as above, you can simplify this function without using try/catch & async
         await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
     } catch (error) {
         throw error;
     }
 };
+
+//your original method of exporting each function as module.exports may be the better option
